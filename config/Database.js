00ba5754ff42949c5e91379ceed78532f4729339
Carlos Proença <carlos_proenca@live.com>
2017-01-31 01:47:43 -0200

    Configuring new database URL

--- config/Database.js
+++ config/Database.js
@@ -2,6 +2,6 @@
 const mongorito = require('mongorito');
 
 //the connection URL
-const DATABASE_URL = 'mongodb://cashUserDB:12345@ds013966.mlab.com:13966/heroku_zfbbt2rg';
+const DATABASE_URL = 'mongodb://cashUserDB:12345@ds137759.mlab.com:37759/heroku_wgs0m13f';
 
 module.exports = mongorito.connect(DATABASE_URL);
